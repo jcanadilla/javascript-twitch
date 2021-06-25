128c81475b8913e919a1b1626ae5d14bd492feb9
Javier Cañadilla Casco <jcanadillacasco@outlook.com>
2021-06-25 23:00:13 +0000

    Initial commit with jest

---
+++ no-modificar/variables.js
@@ -0,0 +1,27 @@
+/*
+  ¿Qué es una variable?
+  - Es un espacio de memoria del programa que tiene un identificador
+
+  ¿Qué es declarar una variable?
+  - Es asignar un nombre y/o valor a un espacio de memoria
+
+  ¿Cómo declaro una variable en javascript?
+  - Puedes utilizar "let" o "const"
+
+  ¿Qué diferencia hay entre "let" y "const"?
+  - Principalmente, una variable declarada con "let" puede cambiar de valor. Mientras
+    que si lo hacemos con "const" esta no podrá cambiar de valor. Es decir, será CONSTANTE
+  
+  ¿Qué es un el tipo de una variable?
+  - Es la clase de dato que se puede almacenar en ella. Ej: número, texto, verdadero/falso...
+*/
+
+// Declaramos una variable constante
+const a = 1;
+
+// Declaramos una variable
+let b = 2;
+
+// Operaciones con variables
+let c = a + b; // ¿Cuál será el valor de c?
+
